e3fb4e676a1cc48b6278bc8b0eaebddbd7982d12
Tobias Leinss <caruso33@web.de>
2022-09-04 11:56:05 +0200

    Add data and read it

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { createContract, showContracts } from "./api"
 import { CreateContractData, ShowContractsData } from "./types"
+import fs from "fs"
+import path from "path"
 
 async function createNftContract(nftData: CreateContractData): Promise<void> {
   try {
@@ -23,8 +25,6 @@ async function showNftContracts(
 ): Promise<any | void> {
   try {
     const { data } = await showContracts(nftData)
-    console.log("Found following contracts: ")
-    console.log(data?.contracts)
 
     return data?.contracts
   } catch (e: any) {
@@ -32,24 +32,50 @@ async function showNftContracts(
   }
 }
 
+async function readData(dirPath: string) {
+  const dirFiles = await fs.readdirSync(dirPath)
+
+  const jsonData = dirFiles.filter((fileName) => fileName.endsWith(".json"))
+
+  const parsedJsonData: Record<string, any>[] = []
+
+  for (let fileName of jsonData) {
+    const fileContent = fs.readFileSync(path.join(dirPath, fileName), {
+      encoding: "utf-8",
+    })
+
+    const parsedJson: Record<string, any> = JSON.parse(fileContent)
+    parsedJsonData.push(parsedJson)
+  }
+
+  return parsedJsonData
+}
+
 async function main() {
   const chainId = 80001 // mumbai
   const name = "Thentic Tobias Leinss Application"
   const shortName = "TTLA"
 
-  const nftData = { chainId, name, shortName }
+  const nftContractData = { chainId, name, shortName }
 
-  // await createNftContract(nftData)
+  // await createNftContract(nftContractData)
 
   const contractData = await showNftContracts({ chainId })
 
   if (!contractData || contractData?.length === 0) {
     console.error(`No contract data found`)
     process.exit(0)
+  } else {
+    console.log("Found following contracts: ")
+    console.log(contractData)
   }
 
   const contractAddress = contractData[0].contract
   console.log(`Contract address is: ${contractAddress}`)
+
+  const dirPath = "./data"
+  const nftData = await readData(dirPath)
+  console.dir(nftData)
 }
 
 main()
