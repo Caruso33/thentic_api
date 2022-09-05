d303d04a51716758c571f19aea7cf48f8337afa0
Tobias Leinss <caruso33@web.de>
2022-09-05 13:16:48 +0200

    Add more comments

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,15 @@ async function main() {
   const ownerAddress = "0x4bFC74983D6338D3395A00118546614bB78472c2"
   const transferToAddress = "0x6a439b14f527d8731794B982d785b72F5d245c6f"
 
+  //
+  // create nft contract
+  //
+
   // await createNftContract(nftContractData)
+
+  //
+  // fetch nft contracts for given key
+  //
 
   const contractData = await showNftContracts({ chainId })
 
@@ -33,13 +41,19 @@ async function main() {
   const contractAddress = contractData[0].contract
   console.log(`Contract address is: ${contractAddress}`)
 
+  //
+  // read local nft data to mint them
+  //
+
   const dirPath = "./data"
   const nftData = await readData(dirPath)
   console.log(`Nft Data found in path: ${JSON.stringify(nftData, null, 2)}`)
 
   const currentNfts = await showCurrentNfts(nftContractData)
 
+  //
   // Mint Nft
+  //
 
   // const nftMintingData = {
   //   chainId,
@@ -50,7 +64,9 @@ async function main() {
 
   // await mintNfts(nftData, nftMintingData)
 
+  //
   // Transfer Nft
+  //
 
   // const nftToTransfer = 1
   // const nftTransferData = {
